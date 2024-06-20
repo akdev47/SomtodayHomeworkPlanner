006cd1384aeb74845d67720fae7ef6ef86d56bd7
akuysal <a.k.uysal@student.utwente.nl>
2024-06-20 17:10:55 +0200

    Added admin, sidebar colors, modified calendar, classes view switch, and individual class backend!

--- src/main/java/somtoday/model/Lesson.java
+++ src/main/java/somtoday/model/Lesson.java
@@ -11,6 +11,11 @@ public class Lesson {
 
     private int schoolYear;
 
+
+    private String lessonName;
+
+    private String lessonDescription;
+
     public Lesson() { }
 
     public Lesson(int lessonID, int courseID, int classID, int schoolYear) {
@@ -50,5 +55,21 @@ public class Lesson {
 
     public void setSchoolYear(int schoolYear) {
         this.schoolYear = schoolYear;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public String getLessonDescription() {
+        return lessonDescription;
+    }
+
+    public void setLessonDescription(String lessonDescription) {
+        this.lessonDescription = lessonDescription;
     }
 }
