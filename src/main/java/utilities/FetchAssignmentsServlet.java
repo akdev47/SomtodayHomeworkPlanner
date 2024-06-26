8b95b1a1c3800c5cbc1b0663df3f6d7a924ecfc8
Taylan Kincir <a.t.kincir@student.utwente.nl>
2024-06-26 22:23:12 +0200

    new interface for assignments is added. assignments-of-class.html is still missing splits.

--- src/main/java/utilities/FetchAssignmentsServlet.java
+++ src/main/java/utilities/FetchAssignmentsServlet.java
@@ -41,31 +41,27 @@ public class FetchAssignmentsServlet extends HttpServlet {
 
             String sql;
             if ("teacher".equals(role)) {
-                sql = "WITH HomeworkCTE AS (\n" +
-                        "    SELECT \n" +
-                        "        h.class_id, \n" +
-                        "        h.homework_name, \n" +
-                        "        h.publish_date, \n" +
-                        "        h.due_date, \n" +
-                        "        h.teacher_id, \n" +
-                        "        h.homework_id,\n" +
-                        "        ROW_NUMBER() OVER (PARTITION BY h.class_id, h.homework_name, h.publish_date, h.due_date, h.teacher_id \n" +
-                        "                           ORDER BY h.homework_id) AS rn\n" +
-                        "    FROM homework h\n" + ")\n" + "SELECT \n" + "    c.class_name, \n" + "    HomeworkCTE.homework_name, \n" + "    HomeworkCTE.publish_date, \n" + "    HomeworkCTE.due_date, \n" + "    HomeworkCTE.homework_id\n" + "FROM HomeworkCTE\n" + "JOIN class c ON HomeworkCTE.class_id = c.class_id\n" + "JOIN teacher t ON HomeworkCTE.teacher_id = t.teacher_id\n" + "JOIN person p ON t.person_id = p.person_id\n" + "WHERE HomeworkCTE.rn = 1\n" + "AND t.person_id = " + personId + " ORDER BY HomeworkCTE.publish_date DESC;";
+                sql = "SELECT DISTINCT c.class_name, h.homework_id, h.homework_name, h.publish_date, h.due_date " +
+                        "FROM homework h " +
+                        "JOIN class c ON h.class_id = c.class_id " +
+                        "JOIN teacher t ON h.teacher_id = t.teacher_id " +
+                        "WHERE h.student_id IS NULL " +
+                        "AND t.person_id = " + personId +
+                        " ORDER BY h.publish_date DESC;";
             } else if ("student".equals(role)) {
-                sql = "SELECT DISTINCT c.class_name, h.homework_id, h.homework_name, h.publish_date, h.due_date "  +
+                sql = "SELECT DISTINCT c.class_name, h.homework_id, h.homework_name, h.publish_date, h.due_date " +
                         "FROM homework h, student s, person p, class c " +
                         "WHERE h.student_id = s.student_id " +
                         "AND h.class_id = c.class_id " +
                         "AND s.person_id = " + personId +
-                        "ORDER BY h.publish_date DESC";
-
-            }  else if ("admin".equals(role)) {
+                        " ORDER BY h.publish_date DESC;";
+            } else if ("admin".equals(role)) {
                 sql = "SELECT DISTINCT c.class_name, h.homework_id, h.homework_name, h.publish_date, h.due_date " +
-                        "FROM somtoday6.homework h, class c " +
-                        "WHERE h.class_id = c.class_id " +
-                        "ORDER BY h.publish_date DESC";
-            }  else {
+                        "FROM homework h " +
+                        "JOIN class c ON h.class_id = c.class_id " +
+                        "WHERE h.student_id IS NULL " +
+                        "ORDER BY h.publish_date DESC;";
+            } else {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid role");
                 return;
             }
@@ -93,32 +89,32 @@ public class FetchAssignmentsServlet extends HttpServlet {
         }
 
         PrintWriter out = response.getWriter();
-        out.write(toJson(classList,assignmentList));
+        out.write(toJson(classList, assignmentList));
         out.close();
     }
 
-    private String toJson(List<Class> classList,List<Homework> assignmentList) {
+    private String toJson(List<Class> classList, List<Homework> assignmentList) {
         StringBuilder json = new StringBuilder();
         json.append("[");
-        if (classList.size() == assignmentList.size()){
-        for (int i = 0; i < assignmentList.size(); i++) {
-            Homework hw = assignmentList.get(i);
-            Class c = classList.get(i);
-            json.append("{");
-            json.append("\"class_name\":\"").append(c.getClassName()).append("\",");
-            json.append("\"homework_id\":").append(hw.getHomeworkID()).append(",");
-            json.append("\"homework_name\":\"").append(hw.getHomeworkName()).append("\",");
-            json.append("\"publish_date\":\"").append(hw.getPublishDate()).append("\",");
-            json.append("\"due_date\":\"").append(hw.getDueDate()).append("\"");
-            json.append("}");
+        if (classList.size() == assignmentList.size()) {
+            for (int i = 0; i < assignmentList.size(); i++) {
+                Homework hw = assignmentList.get(i);
+                Class c = classList.get(i);
+                json.append("{");
+                json.append("\"class_name\":\"").append(c.getClassName()).append("\",");
+                json.append("\"homework_id\":").append(hw.getHomeworkID()).append(",");
+                json.append("\"homework_name\":\"").append(hw.getHomeworkName()).append("\",");
+                json.append("\"publish_date\":\"").append(hw.getPublishDate()).append("\",");
+                json.append("\"due_date\":\"").append(hw.getDueDate()).append("\"");
+                json.append("}");
 
-            if (i < assignmentList.size() - 1) {
-                json.append(",");
+                if (i < assignmentList.size() - 1) {
+                    json.append(",");
+                }
             }
-        }
 
-        json.append("]");
-        return json.toString();
+            json.append("]");
+            return json.toString();
         } else {
             return "ERROR IN THE SERVER";
         }
